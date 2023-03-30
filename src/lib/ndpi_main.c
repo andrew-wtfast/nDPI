d7e40a01b16552676cf822560b9ea3a368fcb9f4
Andrew Parker <andrew@wtfast.com>
2023-03-29 17:25:37 -0700

    CoD preliminary based on a single xbox pcap and a single ps4 pcap

--- src/lib/ndpi_main.c
+++ src/lib/ndpi_main.c
@@ -2059,6 +2059,14 @@ static void ndpi_init_protocol_defaults(struct ndpi_detection_module_struct *ndp
 			  ndpi_build_default_ports(ports_b, 41641, 0, 0, 0, 0) /* UDP */);
 
 
+  ndpi_set_proto_defaults(ndpi_str, 1 /* cleartext */, 0 /* nw proto */, NDPI_PROTOCOL_SAFE, NDPI_PROTOCOL_COD,
+			  "COD", NDPI_PROTOCOL_CATEGORY_GAME,
+			  ndpi_build_default_ports(ports_a, 0, 0, 0, 0, 0) /* TCP */,
+			  ndpi_build_default_ports(ports_b, 0, 0, 0, 0, 0) /* UDP */);
+
+
+
+
 #ifdef CUSTOM_NDPI_PROTOCOLS
 #include "../../../nDPI-custom/custom_ndpi_main.c"
 #endif
@@ -4887,6 +4895,9 @@ static int ndpi_callback_init(struct ndpi_detection_module_struct *ndpi_str) {
 
   /* Tailscale */
   init_tailscale_dissector(ndpi_str, &a);
+
+  /* COD */
+  init_cod_dissector(ndpi_str, &a);
 
 #ifdef CUSTOM_NDPI_PROTOCOLS
 #include "../../../nDPI-custom/custom_ndpi_main_init.c"
